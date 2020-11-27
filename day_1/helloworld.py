df44ed30c2746c28b0506df343c41b4b05b398e6
ErassMirza <erass.mirza@gmail.com>
2020-11-27 20:36:07 +0500

    Initial commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,24 @@
+# 2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+# 3
+print('Erass')
+print("Mirza")
+print('''Pakistan''')
+print('''I am enjoying 30 days of Python''')
+
+# 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Erass',"Python",'''Finland''']))
+print(type('Erass'))
+print(type("Mirza"))
+print(type('''Pakistan'''))
