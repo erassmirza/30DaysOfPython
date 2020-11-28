95f8e6eecd044b6a26df7976c0faed5579677399
erassmirza <erass.mirza@gmail.com>
2020-11-28 13:35:45 +0500

    Day 2 done

---
+++ day_2/variables.py
@@ -0,0 +1,87 @@
+""" Exercises: Level 1 """
+
+
+# 2
+# Day 2: 30 Days of python programming
+
+# 3
+first_name = 'Erass'
+
+# 4
+last_name = 'Mirza'
+
+# 5
+full_name = "Erass Mirza"
+
+# 6
+country = "Pakistan"
+
+# 7
+city = 'Karachi'
+
+# 8
+age = 22
+
+# 9
+year = 2020
+
+# 10
+is_married = False
+
+# 11
+is_true = True
+
+# 12
+is_light_on = False
+
+# 13
+varA, varB, varC = 'String', 10, True
+
+
+""" Exercises: Level 2 """
+
+
+# 1
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(varA))
+print(type(varB))
+print(type(varC))
+
+# 2
+print(len(first_name))
+
+# 3
+print(len(first_name)==len(last_name))
+
+# 4
+num_one, num_two = 5,4
+_total = num_one + num_two
+_diff = num_one - num_two
+_product = num_one * num_two
+_division = num_one / num_two
+_reminder = num_one % num_two
+_exp = num_one ** num_two
+_floor_division = num_one // num_two
+
+# 5
+radius_of_circle = 30
+area_of_circle = 3.142 * radius_of_circle ** 2
+circum_of_circle = 2 * 3.142 * radius_of_circle
+radiusOfCircle = int(input('Enter the radius of Circle: '))
+areaOfCircle = 3.142 * radiusOfCircle ** 2
+print('Area of Circle is: ',areaOfCircle)
+
+# 6
+firstName = input('Enter your first name: ')
+lastName = input('Enter your last name: ')
+country = input('Enter your country: ')
+age = input('Enter your age: ')
